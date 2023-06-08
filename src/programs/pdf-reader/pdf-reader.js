a89c0980d7f6516ca59ee06f2c7806db57026a18
Hadas Tamás <hadastk@gmail.com>
2023-06-09 01:57:06 +0200

    Update pdf-reader and hungarian CV

--- src/programs/pdf-reader/pdf-reader.js
+++ src/programs/pdf-reader/pdf-reader.js
@@ -16,9 +16,21 @@ export default {
         }
     ],
     name: 'PDF Reader',
-    run: (window, file) => {
-        window.name += ` - ${file.name}`;
-        window.querySelector('main').innerHTML = `<iframe src="./src/${file.path}.html"></iframe>`;
+    run: async (window, file) => {
+        const {default: data} = await import(`/src/${file.path}.js`);
+        const inner = document.createElement('div');
+        const main = window.querySelector('main');
+        const style = document.createElement('style');
+
+        inner.className = data.className;
+        inner.id = 'body';
+        inner.innerHTML = data.html;
+        style.innerHTML = data.css;
+        main.attachShadow({mode: 'open'});
+        main.shadowRoot.append(style, inner);
+
+        window.maximize();
+        window.windowName += ` - ${file.name}`;
     },
     shortcut: false
     // mely ablak műveletek érhetőek el (hide, disabled)
