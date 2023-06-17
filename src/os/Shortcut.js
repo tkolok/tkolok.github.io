882af6b46f8281c8794c9c9bcccc0b96ae6ac3b2
Hadas Tamás <hadastk@gmail.com>
2023-06-18 00:47:58 +0200

    Allow to create singleton program

--- src/os/Shortcut.js
+++ src/os/Shortcut.js
@@ -1,12 +1,11 @@
-import {get, getConfig} from './programs.js';
+import {getConfig, open} from './programs.js';
 
 export default class Shortcut extends HTMLDivElement {
     constructor(config, data = null) {
         super();
-        const Program = get(config.id);
         const programConfig = getConfig(config.id);
 
-        this.addEventListener('dblclick', () => new Program(data));
+        this.addEventListener('dblclick', () => open(config.id, data));
         this.classList.add('shortcut');
         this.innerHTML = `
             <div class="icon ${config.icon || programConfig.icon || ''}"></div>
--- src/os/programs.js
+++ src/os/programs.js
@@ -4,15 +4,40 @@ export async function define(module) {
     const {default: program, config} = await module;
 
     customElements.define(`w-program-${config.id}`, program, {extends: 'dialog'});
-    programs.set(config.id, {config, program});
+    programs.set(config.id, buildData(config, program));
 
     return module;
 }
 
-export function get(id) {
-    return programs.get(id).program;
-}
-
 export function getConfig(id) {
     return programs.get(id).config;
+}
+
+export function open(id, data) {
+    return programs.get(id).open(data);
+}
+
+function buildData(config, program) {
+    let openProgram;
+
+    if (config.once) {
+        let instance;
+
+        openProgram = data => {
+            if (!instance?.isConnected) {
+                instance = new program(data);
+            } else {
+                instance.active = true;
+            }
+
+            return instance;
+        };
+    } else {
+        openProgram = data => new program(data);
+    }
+
+    return {
+        config,
+        open: openProgram
+    };
 }
