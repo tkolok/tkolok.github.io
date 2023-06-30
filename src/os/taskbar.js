e3a415c9d0240b462b82f07e60bb4425bc440740
Hadas Tamás <hadastk@gmail.com>
2023-06-30 23:02:49 +0200

    Allow to add children in start menu

--- src/os/taskbar.js
+++ src/os/taskbar.js
@@ -1,71 +1,78 @@
 import query from '../common/query.js';
+import {open} from './programs.js';
 
 const {dateAndTime, startButton, startMenu} = query(document.body.querySelector('#taskbar'));
 const dateFormatter = new Intl.DateTimeFormat(undefined, {timeStyle: 'short'});
 
-startButton.addEventListener('blur', () => toggleStartMenu(false));
+startButton.addEventListener('blur', blur);
 startButton.addEventListener('click', () => toggleStartMenu());
 updateTime();
 setInterval(updateTime, 1000);
 
-[
-    {
-        click: () => console.log('TODO'),
-        icon: 'programs',
-        label: 'Programs'
-    },
-    {
-        click: () => console.log('TODO'),
-        icon: 'documents',
-        label: 'Documents'
-    },
-    {
-        click: () => console.log('TODO'),
-        icon: 'settings',
-        label: 'Settings'
-    },
-    {
-        click: () => console.log('TODO'),
-        icon: 'find',
-        label: 'Find'
-    },
-    {
-        click: () => console.log('TODO'),
-        icon: 'help',
-        label: 'Help'
-    },
-    {
-        click: () => console.log('TODO'),
-        icon: 'run',
-        label: 'Run...'
-    },
-    null,
-    {
-        click: () => console.log('TODO'),
-        icon: 'log-off',
-        label: 'Log Off...'
-    },
-    {
-        click: () => window.close(),
-        icon: 'monitor',
-        label: 'Shut down...'
+startMenu.append(
+    ...[
+        {
+            children: [
+                {
+                    click: () => console.log('TODO'),
+                    label: 'Accessories'
+                },
+                {
+                    click: () => open('ie'),
+                    icon: 'ie',
+                    label: 'Internet Explorer'
+                },
+                {
+                    click: () => open('notepad'),
+                    icon: 'notepad',
+                    label: 'Notepad'
+                }
+            ],
+            label: 'Programs'
+        },
+        {
+            click: () => open('explorer', '/Documents'),
+            icon: 'documents',
+            label: 'Documents'
+        },
+        {
+            click: () => console.log('TODO'),
+            icon: 'settings',
+            label: 'Settings'
+        },
+        {
+            click: () => console.log('TODO'),
+            icon: 'search-file',
+            label: 'Find'
+        },
+        {
+            click: () => console.log('TODO'),
+            icon: 'help',
+            label: 'Help'
+        },
+        {
+            click: () => console.log('TODO'),
+            icon: 'run',
+            label: 'Run...'
+        },
+        null,
+        {
+            click: () => console.log('TODO'),
+            icon: 'log-off',
+            label: 'Log Off...'
+        },
+        {
+            click: () => window.close(),
+            icon: 'monitor',
+            label: 'Shut down...'
+        }
+    ].map(buildMenuitem)
+);
+
+function blur(event) {
+    if (!event.relatedTarget?.closest('#startMenu')) {
+        toggleStartMenu(false);
     }
-].forEach(addMenuitem);
-
-export function addMenuitem(config) {
-    const li = document.createElement('li');
-
-    if (config) {
-        li.addEventListener('click', buildClick(config));
-        li.classList.add('menuitem');
-        li.innerHTML = `
-            <span class="icon ${config.icon}"></span>
-            <label>${config.label}</label>`;
-    } else {
-        li.classList.add('menu-separator');
-    }
-
-    startMenu.append(li);
 }
 
 function buildClick({click}) {
@@ -73,6 +80,32 @@ function buildClick({click}) {
         toggleStartMenu(false);
         click(event);
     };
+}
+
+function buildMenuitem(config) {
+    const menuitem = document.createElement('li');
+
+    if (config) {
+        menuitem.classList.add('menuitem');
+        menuitem.innerHTML = `
+            <span class="icon ${config.icon || 'programs'}"></span>
+            <label>${config.label}</label>`;
+        menuitem.tabIndex = 1;
+
+        if (config.children) {
+            const ul = document.createElement('ul');
+            ul.append(...config.children.map(buildMenuitem));
+            menuitem.append(ul);
+        }
+
+        if (config.click) {
+            menuitem.addEventListener('click', buildClick(config));
+        }
+    } else {
+        menuitem.classList.add('menu-separator');
+    }
+
+    return menuitem;
 }
 
 function toggleStartMenu(value = !startMenu.classList.contains('open')) {
