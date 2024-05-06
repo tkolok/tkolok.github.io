ed3d539c7ef8f7bff5912a3f4a48ee1a9efc7fed
Hadas Tamás <hadastk@gmail.com>
2024-05-06 22:14:14 +0200

    Add demonstration video URL in comment

--- src/os/folders.js
+++ src/os/folders.js
@@ -70,6 +70,7 @@ const folders = folder(
                         src: 'https://lh3.googleusercontent.com/pw/AP1GczMR4LVa90NSr232l3TkORRWNw_1sEwwKtrqK8oZhjVjAorixDKAmxJRiEsiudztHiHdNEMGQeUVbKYtrNRYEhi0qZX--uqk2_Ns2CIHy0lEtv5r9AtqXnkp-3ihRHd8FQWdzKu0Nn4WtKRzDJUu1l6h=w1222-h629-s-no-gm'
                     },
                     name: 'Demonstration'
+                    // https://youtu.be/KXa5VDHLmEk?t=275
                 }
             )
         ),
