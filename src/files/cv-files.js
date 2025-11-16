c663929e36317af10013df4e3d678a5921268a4a
Hadas Tamás <hadastk@gmail.com>
2025-11-16 20:30:25 +0100

    Update CV

--- src/files/cv-files.js
+++ src/files/cv-files.js
@@ -6,6 +6,8 @@ const translations = {
             'purpose was to create a common component library for the 4 frontend frameworks used within the company. We used Storybook to document this.',
         computerScience: 'Computer science (BSc)',
         contacts: 'Contacts',
+        cvWebpageDescription: 'Just open it! Believe me, you won\'t regret it!',
+        cvWebpageTitle: 'My webpage',
         deik: 'University of Debrecen',
         dynataSummary1:
             'The company has a huge database that includes a versatile UI based on AngularJS. My responsibilities included maintaining it, adding new ' +
@@ -22,11 +24,22 @@ const translations = {
         dynataSummary6:
             'I have been a regular speaker at several professional events (eg. conferences, meetups) and have been mentoring beginners in the field as well.',
         english: 'English',
+        hadasUtEpkerDescription: 'Website created for my brother\'s business.',
         informaticsMajor: 'Informatics major',
-        hobbyProjects: 'Hobby projects',
+        intuitech:
+            'We have launched 2 applications, which I helped to implement: <a href=\"https://scportal.granitbank.hu/csok-plusz\">CSOK+</a> and ' +
+            '<a href=\"https://scportal.granitbank.hu/lakasbiztositas\">Házmester home insurance</a>. Additionally, I worked on a similar, non-production ' +
+            'application and a chat bot frontend. Furthermore, my main tasks include the modernization of existing applications and the correction of ' +
+            'emerging errors. During development, we consistently use at least Angular 17, Angular Material, and Tailwind.',
         languageSkills: 'Language skills',
         mobxPieDescription: 'An example repository is how to use MobX version 3.X with React.',
         name: 'Tamás Hadas',
+        onkenteskednekDescription:
+            'My website is currently in the early stages of development. It is designed to allow users to search for and apply to volunteer opportunities ' +
+            'posted by organizations. The project is built on Angular, Firebase, and Netlify.',
+        otherProjects: 'Other projects',
+        photoBottom: 'but this picture is awesome.',
+        photoTop: 'I lost my sunglasses, I don\'t have mustache,',
         present: 'present',
         professionalExperience: 'Professional experience',
         programmingSkills: 'Programming skills',
@@ -42,6 +55,8 @@ const translations = {
             'dokumentáláshoz Storybookot használtunk.',
         computerScience: 'Programtervező informatikus (BSc)',
         contacts: 'Elérhetőségek',
+        cvWebpageDescription: 'Csak nyisd meg! Hidd el, nem fogod megbánni!',
+        cvWebpageTitle: 'A weboldalam',
         deik: 'Debreceni Egyetem Informatikai Kar',
         dynataSummary1:
             'A cég rendelkezik egy hatalmas adatbázissal, amelyhez tartozik egy sokrétű UI, amely AngularJS-re épül. A feladataim közé tartozott ennek ' +
@@ -58,11 +73,22 @@ const translations = {
             'hozzátartozó adatbázis karbantartása, fejlesztése.',
         dynataSummary6: 'Rendezvényeken rendszeres előadó voltam és mentorálással is foglalkoztam.',
         english: 'Angol',
-        hobbyProjects: 'Hobbi projektek',
+        hadasUtEpkerDescription: 'Testvérem vállalkozásához készített honlap.',
         informaticsMajor: 'Informatika szak',
+        intuitech:
+            'Ez idáig 2 alkalmazást indítottunk útnak, amik megvalósításában segítettem: <a href="https://scportal.granitbank.hu/csok-plusz">CSOK+</a> és ' +
+            '<a href="https://scportal.granitbank.hu/lakasbiztositas">Házmester lakásbiztosítás</a>. Továbbá egy ezekhez hasonló, még nem éles alkalmazáson ' +
+            'és egy chat bot frontendjén dolgoztam. Ezen kívül főbb feladataim közé tartozik a már meglévő alkalmazások modernizálása, és a felbukkanó hibák ' +
+            'javítása. A fejlesztések során egységesen legalább Angular 17-et, Angular Materialt és Tailwindet használunk.',
         languageSkills: 'Nyelvismeretek',
         mobxPieDescription: 'Egy példa repository, hogy (szerintem) hogyan érdemes a MobX 3.X verzióját React-tal használni.',
         name: 'Hadas Tamás',
+        onkenteskednekDescription:
+            'Korai fejlesztési fázisban lévő weboldalam, ahol szervezetek által meghirdetett önkéntes munkák közt lehet keresni, és jelentkezni rájuk. ' +
+            'Angular, Firebase és Netlify alapokra épül a projekt.',
+        otherProjects: 'Egyéb projektek',
+        photoBottom: 'de ez a kép király.',
+        photoTop: 'A napszemüvegem elvesztettem, bajszom nincs,',
         present: 'jelenleg',
         professionalExperience: 'Szakmai tapasztalat',
         programmingSkills: 'Programozási ismeretek',
@@ -86,39 +112,70 @@ function build(lang) {
     body.innerHTML = `
         <aside class="flex flex-column">
             <img alt="" src="./assets/images/cv.jpg">
-                <div>
-                    <h2>${translation.programmingSkills}</h2>
-                    <p>AngularJS, Angular, React, JavaScript, Typescript, CSS (Sass, Less), Java, MSSQL, Jira, TeamCity</p>
-                </div>
-                <div class="flex-fill">
-                    <h2>${translation.languageSkills}</h2>
-                    <ul>
-                        <li>
-                            <div class="language-skills flex flex-row flex-start">
-                                <span class="flex-fill">${translation.english}</span>
-                                <div class="flex-fill">B2</div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>${translation.contacts}</h2>
-                    <ul>
-                        <li>
-                            <div class="contact text-center">
-                                <a href="https://tkolok.github.io/">
-                                    <i class="fa fa-link"></i>
-                                </a>
-                                <a href="https://github.com/tkolok">
-                                    <i class="fa fa-github"></i>
-                                </a>
-                                <a href="https://www.linkedin.com/in/tam%C3%A1s-hadas-a009bb13a/">
-                                    <i class="fa fa-linkedin-square"></i>
-                                </a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+            <svg id="pictureText"
+                 viewBox="0 0 260 280"
+                 xmlns="http://www.w3.org/2000/svg"
+                 width="260"
+                 height="280">
+                <defs>
+                    <path id="circleTop"
+                          d="M 4 138 a 1 1 0 1 1 250 0">
+                    </path>
+                    <path id="circleBottom"
+                          d="M 0 146 a 125 125 0 0 0 260 0">
+                    </path>
+                </defs>
+                <text font-size="14">
+                    <textPath href="#circleTop"
+                              startOffset="50%"
+                              text-anchor="middle">
+                        ${translation.photoTop}
+                    </textPath>
+                </text>
+                <text font-size="14">
+                    <textPath href="#circleBottom"
+                              startOffset="50%"
+                              text-anchor="middle">
+                        ${translation.photoBottom}
+                    </textPath>
+                </text>
+            </svg>
+            <div>
+                <h2>${translation.programmingSkills}</h2>
+                <p>Angular, Angular Material, AngularJS, React, JavaScript, Typescript, Bootstrap, Tailwind, CSS (Sass, Less), Java, MSSQL, Jira, TeamCity</p>
+            </div>
+            <div class="flex-fill">
+                <h2>${translation.languageSkills}</h2>
+                <ul>
+                    <li>
+                        <div class="language-skills flex flex-row flex-start">
+                            <span class="flex-fill">${translation.english}</span>
+                            <div class="flex-fill">B2</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2>${translation.contacts}</h2>
+                <ul>
+                    <li>
+                        <div class="contact text-center">
+                            <a href="https://github.com/tkolok">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 496 512" fill="#2196f3">
+                                    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
+                                </svg>
+                            </a>
+                            <a href="https://www.linkedin.com/in/tam%C3%A1s-hadas-a009bb13a/">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" fill="#2196f3">
+                                    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </aside>
         <main class="flex-fill">
             <h1>${translation.name}</h1>
@@ -126,10 +183,18 @@ function build(lang) {
                 <h2>${translation.professionalExperience}</h2>
                 <section>
                     <h3>
+                        <a href="https://intuitech.studio/">Intuitech</a>
+                        <span>Front End Developer</span>
+                    </h3>
+                    <small>2023 - 2025</small>
+                    <p>${translation.intuitech}</p>
+                </section>
+                <section>
+                    <h3>
                         <a href="https://blackbelt.hu/">BlackBelt</a>
                         <span>Front End Developer</span>
                     </h3>
-                    <small>2021 - ${translation.present}</small>
+                    <small>2021 - 2023</small>
                     <p>${translation.blackBelt}</p>
                 </section>
                 <section>
@@ -174,12 +239,29 @@ function build(lang) {
                 </section>
             </div>
             <div>
-                <h2>${translation.hobbyProjects}</h2>
+                <h2>${translation.otherProjects}</h2>
+                <section>
+                    <h3>
+                        <a href="https://onkenteskednek.hu/">Önkénteskednék</a>
+                    </h3>
+                    <p>${translation.onkenteskednekDescription}</p>
+                </section>
+                <section>
+                    <h3>
+                        <a href="https://tkolok.github.io/">${translation.cvWebpageTitle}</a>
+                    </h3>
+                    <p>${translation.cvWebpageDescription}</p>
+                </section>
+                <section>
+                    <h3>
+                        <a href="https://hadasutepker.hu/">Hadas Út Épker Kft.</a>
+                    </h3>
+                    <p>${translation.hadasUtEpkerDescription}</p>
+                </section>
                 <section>
                     <h3>
                         <a href="https://github.com/tkolok/mobx-pie">MobX Pie</a>
                     </h3>
-                    <small>2017</small>
                     <p>${translation.mobxPieDescription}</p>
                 </section>
             </div>
